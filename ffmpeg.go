356ba60ff445ffa33dbfae7b103dc259e89331f9
Eason Lin <easonlin404@gmail.com>
2018-02-01 17:51:42 +0800

    fix: fix build failed

--- ffmpeg.go
+++ ffmpeg.go
@@ -4,16 +4,14 @@ import (
 	"html/template"
 )
 
-
 type FFmpeg struct {
-	
+
 }
 
 // Excute FFmpeg
 func (f *FFmpeg) Execute(p Profile) error {
-
-
+ return nil
 }
 
 
-var ffmpegTemplate = template.Must(template.New("").Parse("ffmpeg -y -i")
+ var ffmpegTemplate = template.Must(template.New("").Parse("ffmpeg -y -i"))
